4b2456ce853aaf8abe80b674edbad65acdbd3dee
GlennFolker <glenn.folk08@gmail.com>
2023-01-24 17:25:36 +0700

    test

---
+++ avocado_game_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function run(): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core_8e2ab6a7dbd65a98___ops__function__FnMut_______Output______as_wasm_bindgen_a4d9359c04ab850a___closure__WasmClosure___describe__invoke___web_sys_2487697181167c6d___features__gen_FocusEvent__FocusEvent_____(a: number, b: number, c: number): void;
+export function _dyn_core_8e2ab6a7dbd65a98___ops__function__FnMut_____Output______as_wasm_bindgen_a4d9359c04ab850a___closure__WasmClosure___describe__invoke______(a: number, b: number): void;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
